707501ca8519e0454662964adb4710bc7658c478
yuvysingh <Yuvmeister1@gmail.com>
2023-01-09 22:37:19 +0000

    init

--- script.js
+++ script.js
@@ -9,17 +9,39 @@ sumbitButton.addEventListener("click", function () {
 let count = 0;
 
 function createTask() {
+  if (input.value == "") {
+    return;
+  }
   let div = document.createElement("div");
-  div.append(`${input.value}`);
-  div.classList.add("Task");
-  div.setAttribute("id", `${count}`);
-  let button = document.createElement("button");
-  button.append("delete");
-  button.classList.add("delButton");
-  button.setAttribute("btnNumber", `${count}`);
-  div.append(button);
+  div.classList.add("task");
+  div.setAttribute("id", `div${count}`);
+  let text = document.createElement("p");
+  text.append(`${input.value}`);
+  text.classList.add("text");
+
+  let image = document.createElement("img");
+  image.setAttribute("src", "redtrashcan.svg");
+  image.setAttribute("width", "16");
+  image.setAttribute("height", "16");
+  image.setAttribute("id", `image${count}`);
+  count++;
+  let image2 = document.createElement("img");
+  image2.setAttribute("src", "greentick.svg");
+  image2.setAttribute("width", "16");
+  image2.setAttribute("height", "16");
+  image2.setAttribute("id", `image${count}`);
+  div.append(text);
+  div.append(image);
+  div.append(image2);
 
   todoList.append(div);
+  image.addEventListener("click", function () {
+    div.remove();
+  });
+  image2.addEventListener("click", function () {
+    image.remove();
 
+    //
+  });
   input.value = "";
 }
